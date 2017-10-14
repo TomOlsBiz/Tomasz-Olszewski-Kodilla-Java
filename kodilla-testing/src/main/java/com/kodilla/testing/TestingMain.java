0d789d39fda8b4ab3e3f8ce1c5f3cd23aa466d7f
TomOlsBiz <tomolsbiz@gmail.com>
2017-10-14 12:51:51 +0200

    6.2 task final

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
+
+        //This is part of 6.1 module.
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        //This is end of 6.1 module.
+
+        Calculator calculator = new Calculator();
+
+        int addingTest = calculator.add(1,3);
+        if(addingTest==4) {
+            System.out.println("Adding is correct.");
+        } else {
+            System.out.println("Error in adding!");
+        }
+
+        int substractingTest = calculator.substract(7,2);
+        if(substractingTest==5){
+            System.out.println("Substracting is correct!");
+        } else {
+            System.out.println("Error in substracting.");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int substract(int a, int b){
+        return a-b;
+    }
+}
