ab15870041fcf76476e0e86b325bba56752ebe2b
TomOlsBiz <tomolsbiz@gmail.com>
2017-11-17 00:01:12 +0100

    9.2 Good patterns / SOLID+DRY / Product Order Service

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/orderservice/Payment.java
@@ -0,0 +1,20 @@
+package com.kodilla.good.patterns.challenges.orderservice;
+
+public class Payment {
+
+    private String paymentMethod;
+    private double paidAmount;
+
+    public Payment(String paymentMethod, double paidAmount) {
+        this.paymentMethod = paymentMethod;
+        this.paidAmount = paidAmount;
+    }
+
+    public double getPaidAmount() {
+        return paidAmount;
+    }
+}
+
+
+
+
