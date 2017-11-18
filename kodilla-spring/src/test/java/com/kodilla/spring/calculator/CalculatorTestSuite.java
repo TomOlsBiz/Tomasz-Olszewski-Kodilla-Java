ff6ff957f27d3400f181ad6eb20a4ad7bf1d9d1c
TomOlsBiz <tomolsbiz@gmail.com>
2017-11-18 16:39:09 +0100

    10.4 / Spring 5 / @Bean configuration / Calculator

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculator() {
+        //Given
+        ApplicationContext context =  new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double resultAAddB = calculator.add(1,2);
+        double resultASubB = calculator.sub(3,0.5);
+        double resultAMulB = calculator.mul(2, 0.5);
+        double resultADivB = calculator.div(2,2);
+        //Then
+        Assert.assertEquals(3, resultAAddB,0.001);
+        Assert.assertEquals(2.5, resultASubB, 0.001);
+        Assert.assertEquals(1, resultAMulB, 0.001);
+        Assert.assertEquals(1, resultADivB, 0.001);
+    }
+}
